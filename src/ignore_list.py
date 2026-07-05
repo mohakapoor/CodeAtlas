6b89bebc4d62270ad843391ef75575361b705e3c
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-05 23:18:07 +0530

    ignore list

---
+++ src/ignore_list.py
@@ -0,0 +1,44 @@
+IGNORE_DIRS = {
+    ".git",
+    ".venv",
+    "venv",
+    "__pycache__",
+    "node_modules",
+    ".idea",
+    ".vscode",
+    "build",
+    "dist",
+}
+
+IGNORE_EXTENSIONS = {
+    ".pyc",
+    ".pyo",
+    ".so",
+    ".dll",
+    ".exe",
+
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".gif",
+    ".svg",
+    ".ico",
+
+    ".joblib",
+    ".pkl",
+    ".pt",
+    ".pth",
+    ".onnx",
+
+    ".zip",
+    ".tar",
+    ".gz",
+    ".7z",
+
+    ".pdf",     
+}
+
+IGNORE_FILES = {
+    ".DS_Store",
+    "Thumbs.db",
+}
