9733d873cd9e5314c046244932fcceea555df293
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-06 01:40:05 +0530

    new syncresult data class

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,16 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 @dataclass 
 class Chunk:
     id: str
     content :str
     metadata : dict
+
+
+@dataclass
+class SyncResult:
+    repo_name: str
+    repo_path: Path 
+    status: str 
+    changed_files: list[str] | None
