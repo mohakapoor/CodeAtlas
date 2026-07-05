eff262cbdaa3df6168b20a651566e6f3aae481de
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-07-05 18:17:20 +0530

    test file for builidng parser

---
+++ src/parsing/test.py
@@ -0,0 +1,17 @@
+import numpy
+import os
+import math
+
+class Trainer:
+    def train(self,x):
+        return x**x
+
+print("blyat_cyka")
+
+for i in range(5):
+    print(i)
+def load_data():
+    print("loaded data")
+
+for i in range(5):
+    print(i)
